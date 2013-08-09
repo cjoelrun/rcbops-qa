98dfa836d596e7c569bff1569e18724bea46b223
cjoelrun <cameronjlopez@gmail.com>
2013-08-09 11:28:16 -0500

    add yum dependency

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name             'rcbops-qa'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer        "Rackspace US, Inc."
+license           "Apache 2.0"
 description      'Installs/Configures rcbops-qa'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+recipe "default", ""
+%w{ centos ubuntu }.each do |os|
+  supports os
+end
+depends "yum"

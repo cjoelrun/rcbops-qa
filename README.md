480a49a80703a3185ae3abf60f33c2394f0f91be
cjoelrun <cameronjlopez@gmail.com>
2013-08-09 10:49:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# rcbops-qa Cookbook
+
+Base recipes for RCBOPS QA deployments
+
+## Requirements
+
+### Cookbooks
+
+-   yum
+
+### Operating Systems
+
+-   centos
+
+-   ubuntu
+
+## Attributes
+
+None
+
+## Usage
+
+include rcbops-qa::default
---
+++ recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: rcbops-qa
+# Recipe:: default
+#
+# Copyright 2012, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe "yum::epel" if platform_family?('rhel')
+
+package "openssh-clients" do
+  action :install
+end
+
+package "git" do
+  action :install
+end
+
+# replace with firewall cookbook when centos is supported
+execute "disable iptables" do
+  command "/etc/init.d/iptables stop"
+  action :run
+  only_if { platform_family?('rhel') }
+end
